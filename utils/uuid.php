2c4432cb6079647a4e784c5a13d5c0ca1e5ec482
Joel Campos Oliva <78740664+TheMineWayEstudis@users.noreply.github.com>
2022-03-11 16:32:58 +0100

    Allow uuid check by column

--- utils/uuid.php
+++ utils/uuid.php
@@ -1,5 +1,5 @@
 <?php
-  function uuid(string $checkOnTable = "") {
+  function uuid(string $checkOnTable = "", string $field = "id") {
     while(true) {
        $id = vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex(random_bytes(16)), 4));
 
@@ -7,7 +7,7 @@
       if($checkOnTable == "") return $id;
 
       // Se busca si ya hay un registro con este id
-      $count = queryOne("SELECT COUNT(id) as \"count\" FROM $checkOnTable WHERE id = \"$id\";");
+      $count = queryOne("SELECT COUNT($field) as \"count\" FROM $checkOnTable WHERE id = \"$id\";");
       
       if($count["count"] == 0) return $id;
     }
